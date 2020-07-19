ee3f1a6a68609ca08a218fd227d3ea26739c4647
Vadym <webratel@gmail.com>
2020-07-19 03:35:50 +0300

    Вивід MovieListView та MovieDetailView

--- movies/urls.py
+++ movies/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from . import views
-
+from .views import MovieListView, MovieDetailView
 
 urlpatterns = [
-
+    path("movie/", MovieListView.as_view()),
+    path("movie/<int:pk>/", MovieDetailView.as_view()),
 ]
